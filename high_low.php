786c4992ddd23a1175f1290c847fe92ae48473b5
Emartinez511 <mrblonde9993@gmail.com>
2016-04-18 09:51:01 -0500

    I edited the code to allow arguments to be passed so the first number is the min and the second is the max.

--- high_low.php
+++ high_low.php
@@ -1,19 +1,27 @@
 <?php
 
+    $number = mt_rand($argv[1], $argv[2]);
+    $tries = 0;
+    
+    fwrite(STDOUT, "I'm thinking of a number between 1 through 100. Can you guess my number?\n");
 
-    $number = rand(1, 100);
-    $guess = 0;
-    fwrite(STDOUT, "guess number between 1 - 100\n");
+    do {
+        $user = fgets(STDIN) . PHP_EOL;
+        $tries++;
+        if ($user == $number) {
+            echo "Correctomundo. It took you only $tries tries!\n";
+        } elseif ($user < $number) {
+            echo "nope. higher\n";
+        } else {
+            echo "guess again. lower\n";
+        }
+    } while ($number != $user);
 
-do {
-    $user = fgets(STDIN) . PHP_EOL;
-    $guess++;
-    if ($user == $number) {
-        echo "Correctomundo. it took you only $guess tries!\n";
-    } elseif ($user < $number) {
-        echo "higher\n";
+    echo "do you want to play Again?\n";
+    $playAgain = fgets(STDIN) . PHP_EOL;
+    if ($playAgain == "y")   {
+        echo "then write a function for me\n";
     } else {
-        echo "lower\n";
+        echo "bye\n";
     }
 
-} while ($number != $user);
